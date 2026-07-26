faeec0c9543283965b162631857fc9e404e0ce99
carlos cruz <carturo99@gmail.com>
2026-07-26 18:12:43 -0500

    feat(exception): add ForbiddenException

---
+++ src/main/java/dev/ccruz/task_management/exception/ForbiddenException.java
@@ -0,0 +1,8 @@
+package dev.ccruz.task_management.exception;
+
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}

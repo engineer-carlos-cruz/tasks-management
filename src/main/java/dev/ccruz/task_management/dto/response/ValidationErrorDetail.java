160e1a4e2f2e97692e56247edca1bf2e1840e441
carlos cruz <carturo99@gmail.com>
2026-07-30 20:06:26 -0500

    feat: add ValidationErrorDetail DTO for field-level errors

---
+++ src/main/java/dev/ccruz/task_management/dto/response/ValidationErrorDetail.java
@@ -0,0 +1,41 @@
+package dev.ccruz.task_management.dto.response;
+
+public class ValidationErrorDetail {
+
+    private String field;
+    private String message;
+    private Object rejectedValue;
+
+    public ValidationErrorDetail() {
+    }
+
+    public ValidationErrorDetail(String field, String message, Object rejectedValue) {
+        this.field = field;
+        this.message = message;
+        this.rejectedValue = rejectedValue;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getRejectedValue() {
+        return rejectedValue;
+    }
+
+    public void setRejectedValue(Object rejectedValue) {
+        this.rejectedValue = rejectedValue;
+    }
+}
